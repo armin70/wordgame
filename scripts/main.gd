72b3912f96ea11eae2d5aacaba932d58cc034c5b
unknown <armin.akbari.aam91@gmail.com>
2026-05-31 17:58:27 +0330

    bug solved

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,9 +8,9 @@ var turn_active = false # کنترل حرکت تایمر
 @export var max_aspect : float = 1.0
 func _ready():
 	_set_aspect()
-	$gameContainer/UIRoot/TimerBar.max_value = time_left
-	$gameContainer/UIRoot/TimerBar.value = time_left
-	$gameContainer/UIRoot/SubmitButton.pressed.connect(_on_submit_pressed)
+	$gameContainer/game_scene/TimerBar.max_value = time_left
+	$gameContainer/game_scene/TimerBar.value = time_left
+	$gameContainer/game_scene/SubmitButton.pressed.connect(_on_submit_pressed)
 	
 func _set_aspect():
 	var vp_rect = get_viewport_rect()
@@ -26,14 +26,14 @@ func _process(delta):
 
 	time_left -= delta
 
-	$gameContainer/UIRoot/TimerLabel.text = str(int(ceil(time_left)))
-	$gameContainer/UIRoot/TimerBar.value = time_left
+	$gameContainer/game_scene/TimerLabel.text = str(int(ceil(time_left)))
+	$gameContainer/game_scene/TimerBar.value = time_left
 
 	if time_left <= 0:
 		turn_active = false
 		time_left = 0
 		print('time is over')
-		$gameContainer/UIRoot.turn_over()
+		$gameContainer/game_scene.turn_over()
 		
 func reset_timer():
 	print("Timer Reset")
@@ -48,14 +48,14 @@ func _on_submit_pressed():
 
 func _on_clear_pressed():
 	if not game_finished:
-		$gameContainer/UIRoot.clear_current_word()
+		$gameContainer/game_scene.clear_current_word()
 
 
 func restart_game():
 	game_finished = false
 	time_left = 20.0
 	turn_active = false
-	$gameContainer/UIRoot/TimerLabel.text = "20"
+	$gameContainer/game_scene/TimerLabel.text = "20"
 	
 
 
